961a2ff3642bf7757b03902b413e81439f6a7ae1
loctvl842 <loclepnvx@gmail.com>
2026-03-03 09:37:07 +0700

    add highlight

--- lua/beastvim/libs/key/highlights.lua
+++ lua/beastvim/libs/key/highlights.lua
@@ -1,12 +1,30 @@
 local M = {}
 
-M.highlights = {
-	H1 = "IncSearch",
-	H2 = "Bold",
-  Bold = { bold = true },
-  Italic = { italic = true },
-}
+local function set_hl(name, spec)
+	-- If link is provided, use link (don’t mix with fg/bg)
+	if spec.link then
+		vim.api.nvim_set_hl(0, name, { link = spec.link })
+	else
+		vim.api.nvim_set_hl(0, name, spec)
+	end
+end
 
-M.did_setup = false
+function M.defaults()
+	-- Keep these conservative: either links or simple styles
+	return {
+    BeastH2 = { link = "Title" },
+    BeastComment = { link = "Comment" },
+    BeastGroup = { link = "Title" },
+    BeastKeys = { link = "Comment" },
+	}
+end
+
+function M.apply(user_overrides)
+	local groups = vim.tbl_deep_extend("force", M.defaults(), user_overrides or {})
+
+	for name, spec in pairs(groups) do
+		set_hl(name, spec)
+	end
+end
 
 return M
