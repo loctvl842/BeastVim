e3cb3ada4666c878a11f9dc895f637a426099682
loctvl842 <loclepnvx@gmail.com>
2026-05-13 20:26:52 +0700

    stylua

--- init.lua
+++ init.lua
@@ -1,10 +1,10 @@
 if os.getenv("BEAST_PROFILE") == "1" then
-    pcall(function()
-        local profile = require("beast.profile")
-        profile.start()
-        local out = os.getenv("BEAST_PROFILE_OUT") or (vim.fn.stdpath("cache") .. "/beast-profile.txt")
-        profile.auto_dump_on_quit(out)
-    end)
+	pcall(function()
+		local profile = require("beast.profile")
+		profile.start()
+		local out = os.getenv("BEAST_PROFILE_OUT") or (vim.fn.stdpath("cache") .. "/beast-profile.txt")
+		profile.auto_dump_on_quit(out)
+	end)
 end
 
 require("beast").setup()
