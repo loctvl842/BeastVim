883b9f08493966fd25f39ffe10810c6ab187b6ff
loctvl842 <loclepnvx@gmail.com>
2026-08-23 11:47:36 +0700

    chore: remove stray scratch file
    x.lua was an untracked scratch snippet unrelated to any plugin.

--- x.lua
+++
@@ -1,9 +0,0 @@
-local old_name= 'xaolon'
-vim.ui.input({
-  prompt = "New name: ",
-  default = old_name,
-}, function(new_name)
-  if new_name and new_name ~= "" then
-    vim.fn.rename(old_name, new_name)
-  end
-end)
